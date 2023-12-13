315189cb0229657e269c1abc634f18e9da5c6c28
mmxrtine <michellediawara@gmail.com>
2023-12-13 13:16:10 -0800

    0

--- execute_command.c
+++ execute_command.c
@@ -1,39 +1,44 @@
 #include "shell.h"
 
-void execute_command(const char *prompt)
+
+extern char **environ; 
+
+void execute_command(const char *prompt) 
 {
-	pid_t child_pid = fork(); /*create a child process*/
+	int arg_count = 0;
+    	char *args[100];
+	char *paths[] = {"/bin", "/usr/bin", NULL};
+	int i;
 
-	if (child_pid == -1)
+	pid_t child_pid = fork();
+
+    	if (child_pid == -1) 
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
+        perror("fork");
+        exit(EXIT_FAILURE);
 	}
 
-	else if (child_pid == 0) /*child process*/
+    	else if (child_pid == 0) 
 	{
-		/* construct full path */
-		char *path[100];
-		int path_count = 0;
-
-		char *token = strtok((char *)prompt, " ");
-		while (token != NULL && path_count < 99)
-		{
-			path[path_count++] = token;
-			token = strtok(NULL, " ");
-		}
-		path[path_count] = NULL;
-
-		execvp(path[0], path);
-
-		perror("execvp");
-		exit(EXIT_FAILURE);
-	}
-
-	else
+        /* Split the command and arguments */
+        char *token = strtok((char *)prompt, " ");
+        while (token != NULL && arg_count < 99) 
 	{
-		/*parent process*/
-		wait(NULL);
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
 	}
+        args[arg_count] = NULL; /* Null-terminate the array */
 
+
+        /* Try to execute the command with its arguments from specified paths */
+	for ( i = 0; paths[i] != NULL; i++)
+	{	
+	       	char path[100];
+            	strcpy(path, paths[i]);
+            	strcat(path, "/");
+            	strcat(path, args[0]);
+            	execve(path, args, environ);
+	}
 }
+
